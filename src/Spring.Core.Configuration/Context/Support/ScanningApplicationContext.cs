0ba35bf7db7dd7c24bc3eceebc848cf9cadff394
Steve Bohlen <sbohlen@gmail.com>
2011-01-04 16:17:15 -0500

    added missing XML comments

--- src/Spring.Core.Configuration/Context/Support/ScanningApplicationContext.cs
+++ src/Spring.Core.Configuration/Context/Support/ScanningApplicationContext.cs
@@ -19,10 +19,7 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using Spring.Context.Attributes;
 using Spring.Objects.Factory.Support;
 
@@ -68,7 +65,8 @@ namespace Spring.Context.Support
         /// Initializes a new instance of the <see cref="T:Spring.Context.Support.GenericApplicationContext"/> class.
         /// </summary>
         /// <param name="name">The name of the application context.</param><param name="caseSensitive">if set to <c>true</c> names in the context are case sensitive.</param><param name="parent">The parent application context.</param>
-        public ScanningApplicationContext(string name, bool caseSensitive, IApplicationContext parent) : base(name, caseSensitive, parent)
+        public ScanningApplicationContext(string name, bool caseSensitive, IApplicationContext parent)
+            : base(name, caseSensitive, parent)
         {
         }
 
@@ -76,7 +74,8 @@ namespace Spring.Context.Support
         /// Initializes a new instance of the <see cref="T:Spring.Context.Support.GenericApplicationContext"/> class.
         /// </summary>
         /// <param name="objectFactory">The object factory to use for this context</param><param name="parent">The parent applicaiton context.</param>
-        public ScanningApplicationContext(DefaultListableObjectFactory objectFactory, IApplicationContext parent) : base(objectFactory, parent)
+        public ScanningApplicationContext(DefaultListableObjectFactory objectFactory, IApplicationContext parent)
+            : base(objectFactory, parent)
         {
         }
 
@@ -84,29 +83,48 @@ namespace Spring.Context.Support
         /// Initializes a new instance of the <see cref="T:Spring.Context.Support.GenericApplicationContext"/> class.
         /// </summary>
         /// <param name="name">The name of the application context.</param><param name="caseSensitive">if set to <c>true</c> names in the context are case sensitive.</param><param name="parent">The parent application context.</param><param name="objectFactory">The object factory to use for this context</param>
-        public ScanningApplicationContext(string name, bool caseSensitive, IApplicationContext parent, DefaultListableObjectFactory objectFactory) : base(name, caseSensitive, parent, objectFactory)
+        public ScanningApplicationContext(string name, bool caseSensitive, IApplicationContext parent,
+                                          DefaultListableObjectFactory objectFactory)
+            : base(name, caseSensitive, parent, objectFactory)
         {
         }
 
+        /// <summary>
+        /// Scans for types using the provided scanner.
+        /// </summary>
+        /// <param name="scanner">The scanner.</param>
         public void Scan(AssemblyObjectDefinitionScanner scanner)
         {
             scanner.ScanAndRegisterTypes(DefaultListableObjectFactory);
         }
 
+        /// <summary>
+        /// Scans for types using the default scanner.
+        /// </summary>
         public void Scan()
         {
             Scan(new AssemblyObjectDefinitionScanner());
         }
 
-        public  void Scan(Predicate<Type> typePredicate)
+        /// <summary>
+        /// Scans for types that satisfy specified predicate.
+        /// </summary>
+        /// <param name="typePredicate">The type predicate.</param>
+        public void Scan(Predicate<Type> typePredicate)
         {
             Scan(null, ta => true, typePredicate);
         }
 
-        public  void Scan(string assemblyScanPath, Predicate<Assembly> assemblyPredicate, Predicate<Type> typePredicate)
+        /// <summary>
+        /// Scans for types that satisfy specified predicates located in the specified scan path.
+        /// </summary>
+        /// <param name="assemblyScanPath">The assembly scan path.</param>
+        /// <param name="assemblyPredicate">The assembly predicate.</param>
+        /// <param name="typePredicate">The type predicate.</param>
+        public void Scan(string assemblyScanPath, Predicate<Assembly> assemblyPredicate, Predicate<Type> typePredicate)
         {
             //create a scanner instance using the scan path
-            AssemblyObjectDefinitionScanner scanner = new AssemblyObjectDefinitionScanner(assemblyScanPath);
+            var scanner = new AssemblyObjectDefinitionScanner(assemblyScanPath);
 
             //configure the scanner per the provided constraints
             scanner.WithAssemblyFilter(assemblyPredicate).WithIncludeFilter(typePredicate);
@@ -115,18 +133,23 @@ namespace Spring.Context.Support
             Scan(scanner);
         }
 
+        /// <summary>
+        /// Scans for types that satisfy specified predicates.
+        /// </summary>
+        /// <param name="assemblyPredicate">The assembly predicate.</param>
+        /// <param name="typePredicate">The type predicate.</param>
         public void Scan(Predicate<Assembly> assemblyPredicate, Predicate<Type> typePredicate)
         {
             Scan(null, assemblyPredicate, typePredicate);
         }
 
-        public  void Scan(Predicate<Assembly> assemblyPredicate)
+        /// <summary>
+        /// Scans for types that satisfy specified predicate.
+        /// </summary>
+        /// <param name="assemblyPredicate">The assembly predicate.</param>
+        public void Scan(Predicate<Assembly> assemblyPredicate)
         {
             Scan(null, assemblyPredicate, t => true);
         }
-
-
-       
-
     }
 }
