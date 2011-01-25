dbb604b4459111550a5e9282fb2688f67ae837db
Steve Bohlen <sbohlen@gmail.com>
2011-01-25 11:14:03 -0500

    wired up DAILY task to include DOCS compilation

--- build.cmd
+++ build.cmd
@@ -7,7 +7,7 @@ REM 'test-integration'
 cls
 @ECHO building...
 
-tools\nant\bin\nant.exe test -f:Spring.Config.build > buildlog.txt
+tools\nant\bin\nant.exe %1 %2 %3 %4 %5 %6 %7 %8 %9 -f:Spring.Config.build > buildlog.txt
 
 @ECHO displaying log file...
 start "ignored but required placeholder window title argument" buildlog.txt
