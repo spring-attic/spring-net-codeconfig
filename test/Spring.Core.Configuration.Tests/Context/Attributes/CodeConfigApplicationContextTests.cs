45e800ebab795f264022115b589a6759d5ce4544
Steve Bohlen <sbohlen@gmail.com>
2011-04-18 13:27:29 -0400

    adding explicit scanning test for CodeConfigApplicationContext

---
+++ test/Spring.Core.Configuration.Tests/Context/Attributes/CodeConfigApplicationContextTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace Spring.Context.Attributes
+{
+    [TestFixture]
+    public class CodeConfigApplicationContextTests : AbstractConfigurationClassPostProcessorTests
+    {
+
+        protected override void CreateApplicationContext()
+        {
+            GenericApplicationContext ctx = new GenericApplicationContext();
+
+            ctx.ScanAllAssemblies();
+
+            ctx.Refresh();
+
+            _ctx = ctx;
+        }
+
+    }
+
+}
